7f3aa9fe6b7ed0f26c6dd54e0eae131bed201da6
Ferhat Topcu <ferhat@wunderflats.com>
2016-03-04 11:56:13 +0100

    fixing review stuff, tests now run on es5,  changed lint config

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-'use strict';
+'use strict'
 
-var data = require('./data.js'); // data object
-var mappedForRegions = null; // data where regions will be lookup keys
-var mappedForDistricts = null; // data where districts will be lookup keys
-var InvalidPLZ = Error('Invalid Postalcode'); // Invalid PLZ Err
-var WrongFormatError = Error('Wrong param provided'); // Invalid Format Err
-var ParamMissing = Error('Param is missing'); // Param missing err
-var GermanPLZRegex = /^([0]{1}[1-9]{1}|[1-9]{1}[0-9]{1})[0-9]{3}$/; // German PLZ Regex to test
+var data = require('./data.js') // data object
+var mappedForRegions = null // data where regions will be lookup keys
+var mappedForDistricts = null // data where districts will be lookup keys
+var InvalidPostalcode = 'Invalid Postalcode' // Invalid PLZ Err
+var WrongFormatError = 'Wrong param provided' // Invalid Format Err
+var ParamMissing = 'Param is missing' // Param missing err
+var GermanPLZRegex = /^([0]{1}[1-9]{1}|[1-9]{1}[0-9]{1})[0-9]{3}$/ // German PLZ Regex to test
 
 /**
  * Wrapper Function to Map Data to Regions
  * @private
  * @return {Object}
  */
-function createMappedForRegions() {
+function createMappedForRegions () {
   // create empty obj
-  mappedForRegions = {};
+  mappedForRegions = {}
 
   // iterate over all postal codes
   Object.keys(data).forEach(function (plz) {
     // get all regions for one postal code
-    var regions = data[plz].regions;
+    var regions = data[plz].regions
     // iterate over all regions and fill them into result object
     regions.forEach(function (region) {
-      region = region.toLowerCase();
+      region = region.toLowerCase()
 
       // district exists, push into result
       if (mappedForRegions[region]) {
-        mappedForRegions[region].push(plz);
+        mappedForRegions[region].push(plz)
       } else {
         // district does not exist, create array
-        mappedForRegions[region] = [plz];
+        mappedForRegions[region] = [plz]
       }
-    });
-  });
+    })
+  })
 
-  return mappedForRegions;
+  return mappedForRegions
 }
 
 /**
@@ -43,58 +43,57 @@ function createMappedForRegions() {
  * @private
  * @return {Object}
  */
-function createMappedForDistricts() {
+function createMappedForDistricts () {
   // create empty obj
-  mappedForDistricts = {};
+  mappedForDistricts = {}
   // iterate over all postal codes
   Object.keys(data).forEach(function (plz) {
     // get all districts for one postal code
-    var districts = data[plz].districts;
+    var districts = data[plz].districts
 
     // iterate over all districts and fill them into result object
     districts.forEach(function (district) {
-      district = district.toLowerCase();
+      district = district.toLowerCase()
       // district exists, push into result
       if (mappedForDistricts[district]) {
-        mappedForDistricts[district].push(plz);
+        mappedForDistricts[district].push(plz)
       } else {
         // district does not exist, create array
-        mappedForDistricts[district] = [plz];
+        mappedForDistricts[district] = [plz]
       }
-    });
-  });
+    })
+  })
 
-  return mappedForDistricts;
+  return mappedForDistricts
 }
 
 /**
  * Get Regions and Districts to Postalcode
  * @param  {String} postalcode German 5 digit postal code
  * @param {String} property "districts" or "regions" if you want to get one specific property
- * @return {[Object,undefined]} Object with "districts" and "regions" Keys storing their values
+ * @return {[Object]|undefined} Object with "districts" and "regions" Keys storing their values
  */
-function get(postalcode, property) {
+function get (postalcode, property) {
   if (!postalcode) {
     // dirty quick undefined / null / false check
-    throw ParamMissing;
+    throw new Error(ParamMissing + ': postalcode')
   }
 
   if (typeof postalcode === 'number') {
-    postalcode = postalcode.toString();
+    postalcode = postalcode.toString()
   }
 
   if (typeof postalcode === 'string') {
     if (postalcode.match(GermanPLZRegex)) {
       if (property === 'districts' || property === 'regions') {
-        return data[postalcode] ? data[postalcode][property] : [];
+        return data[postalcode] ? data[postalcode][property] : []
       }
-      return data[postalcode];
+      return data[postalcode]
     }
-    throw InvalidPLZ;
+    throw new Error(InvalidPostalcode)
   }
-  return [];
+  return []
 }
-
 
 /**
  * Get Districts by Postalcode
@@ -104,35 +103,35 @@ function get(postalcode, property) {
  * Example: Searchfing for "68159" will not tell you which district in
  * Mannheim the Postal code belongs to but tell you that it belongs to Mannheim
  * @param  {String} postalcode German 5 digit postal code
- * @return {Array<String>}            Array of Districts
+ * @return {Array.<String>}            Array of Districts
  */
-function getDistricts(postalcode) {
-  return get(postalcode, 'districts');
+function getDistricts (postalcode) {
+  return get(postalcode, 'districts')
 }
 
 /**
  * Get Regions (Bundesländer) by Postalcode
  * @param  {String} postalcode German 5 digit postal code
- * @return {Array<String>}            Array of Regions
+ * @return {Array.<String>}            Array of Regions
  */
-function getRegions(postalcode) {
-  return get(postalcode, 'regions');
+function getRegions (postalcode) {
+  return get(postalcode, 'regions')
 }
 
 /**
  * Get All Postal codes for one Region (Bundesland)
  * @param  {String} region The region (Bundesland) e.g "Berlin"
- * @return {Array<String>}        Array of Postalcodes
+ * @return {Array.<String>}        Array of Postalcodes
  */
-function getPostalCodesByRegion(region) {
+function getPostalCodesByRegion (region) {
   if (!region) {
-    throw ParamMissing;
+    throw new Error(ParamMissing + ': region')
   }
   if (typeof region !== 'string') {
-    throw WrongFormatError;
+    throw new Error(WrongFormatError)
   }
-  mappedForRegions = mappedForRegions || createMappedForRegions();
-  return mappedForRegions[region.toLowerCase()] || [];
+  mappedForRegions = mappedForRegions || createMappedForRegions()
+  return mappedForRegions[region.toLowerCase()] || []
 }
 
 /**
@@ -144,17 +143,17 @@ function getPostalCodesByRegion(region) {
  * Search for district first, if your result has length 0, search for the city.
  * If that's 0 again, check for region (Bundesland) - with getPostalCodesByRegion
  * @param  {String} region The District e.g "Gesundbrunnen" (Berlin)
- * @return {Array<String>}        Array of Postalcodes
+ * @return {Array.<String>}        Array of Postalcodes
  */
-function getPostalCodesByDistrict(district) {
+function getPostalCodesByDistrict (district) {
   if (!district) {
-    throw ParamMissing;
+    throw new Error(ParamMissing + ': district')
   }
   if (typeof district !== 'string') {
-    throw WrongFormatError;
+    throw new Error(WrongFormatError)
   }
-  mappedForDistricts = mappedForDistricts || createMappedForDistricts();
-  return mappedForDistricts[district.toLowerCase()] || [];
+  mappedForDistricts = mappedForDistricts || createMappedForDistricts()
+  return mappedForDistricts[district.toLowerCase()] || []
 }
 
 /**
@@ -167,4 +166,4 @@ module.exports = {
   getPostalCodesByRegion: getPostalCodesByRegion,
   getDistricts: getDistricts,
   getRegions: getRegions
-};
+}
--- test/index.js
+++ test/index.js
@@ -1,67 +1,67 @@
 /* global describe, it */
-const assert = require('assert');
-const lib = require('../index');
+var assert = require('assert')
+var lib = require('../index')
 
-describe('Postalcodes.', () => {
-  describe('get()', () => {
-    it('should throw Error in case the PostalCode is in a wrong format', () => {
-      assert.throws(() => lib.get('Hallo'), Error);
-    });
+describe('Postalcodes.', function () {
+  describe('get()', function () {
+    it('should throw Error in case the PostalCode is in a wrong format', function () {
+      assert.throws(function () { lib.get('Hallo') }, Error)
+    })
 
-    it('should throw Error in case the Postalcode is too long or too short', () => {
-      assert.throws(() => lib.get('123122'), Error);
-      assert.throws(() => lib.get('123'), Error);
-    });
+    it('should throw Error in case the Postalcode is too long or too short', function () {
+      assert.throws(function () { lib.get('123122') }, Error)
+      assert.throws(function () { lib.get('123') }, Error)
+    })
 
-    it('should throw Error in case no Postalcode was provided', () => {
-      assert.throws(lib.get, Error);
-    });
+    it('should throw Error in case no Postalcode was provided', function () {
+      assert.throws(lib.get, Error)
+    })
 
-    it('should return Array if valid property (districts, regions) param was provided', () => {
-      assert.equal(Array.isArray(lib.get('13347', 'districts')), true);
-      assert.equal(Array.isArray(lib.get('13347', 'regions')), true);
-    });
+    it('should return Array if valid property param was provided', function () {
+      assert.equal(Array.isArray(lib.get('13347', 'districts')), true)
+      assert.equal(Array.isArray(lib.get('13347', 'regions')), true)
+    })
 
-    it('should return whole result Object in case property param is invalid', () => {
-      assert.equal(lib.get('13347', 'invalidprop'), lib.get('13347'));
-    });
-  });
+    it('should return whole result Object in case property param is invalid', function () {
+      assert.equal(lib.get('13347', 'invalidprop'), lib.get('13347'))
+    })
+  })
 
-  describe('getPostalCodesByDistrict()', () => {
-    it('should throw Error in case no district was provided', () => {
-      assert.throws(lib.getPostalCodesByDistrict, Error);
-    });
+  describe('getPostalCodesByDistrict()', function () {
+    it('should throw Error in case no district was provided', function () {
+      assert.throws(lib.getPostalCodesByDistrict, Error)
+    })
 
-    it('should throw Error in case district is not a String', () => {
-      assert.throws(() => lib.getPostalCodesByDistrict(1234), Error);
-    });
+    it('should throw Error in case district is not a String', function () {
+      assert.throws(function () { lib.getPostalCodesByDistrict(1234) }, Error)
+    })
 
-    it('should provide an Array in case a district was provided', () => {
-      assert.equal(Array.isArray(lib.getPostalCodesByDistrict('Gesundbrunnen')), true);
-    });
+    it('should provide an Array in case a district was provided', function () {
+      assert.equal(Array.isArray(lib.getPostalCodesByDistrict('Gesundbrunnen')), true)
+    })
 
-    it('should provide a filled Array in case a valid district was found', () => {
+    it('should provide a filled Array in case a valid district was found', function () {
       assert.equal(
         lib.getPostalCodesByDistrict('Gesundbrunnen').length > 0, true
-      );
-    });
-  });
+      )
+    })
+  })
 
-  describe('getPostalCodesByRegion()', () => {
-    it('should throw Error in case no region was provided', () => {
-      assert.throws(lib.getPostalCodesByRegion, Error);
-    });
+  describe('getPostalCodesByRegion()', function () {
+    it('should throw Error in case no region was provided', function () {
+      assert.throws(lib.getPostalCodesByRegion, Error)
+    })
 
-    it('should throw Error in case region is not a String', () => {
-      assert.throws(() => lib.getPostalCodesByRegion(1234), Error);
-    });
+    it('should throw Error in case region is not a String', function () {
+      assert.throws(function () { lib.getPostalCodesByRegion(1234) }, Error)
+    })
 
-    it('should provide an Array in case a region was provided', () => {
-      assert.equal(Array.isArray(lib.getPostalCodesByRegion('Berlin')), true);
-    });
+    it('should provide an Array in case a region was provided', function () {
+      assert.equal(Array.isArray(lib.getPostalCodesByRegion('Berlin')), true)
+    })
 
-    it('should provide a filled Array in case a valid region was found', () => {
-      assert.equal(lib.getPostalCodesByRegion('Berlin').length > 0, true);
-    });
-  });
-});
+    it('should provide a filled Array in case a valid region was found', function () {
+      assert.equal(lib.getPostalCodesByRegion('Berlin').length > 0, true)
+    })
+  })
+})
